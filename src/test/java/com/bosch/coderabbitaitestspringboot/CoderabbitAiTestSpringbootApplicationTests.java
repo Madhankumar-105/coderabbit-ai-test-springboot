e760b0d810f1faf397d70876a192d23554874f0e
UMB1COB <madhankumar.subramanian@in.bosch.com>
2025-02-26 10:31:29 +0530

    Initial Commit

---
+++ src/test/java/com/bosch/coderabbitaitestspringboot/CoderabbitAiTestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosch.coderabbitaitestspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderabbitAiTestSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
